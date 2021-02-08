c0743ca717b049a2f36f56c2670123c21ecf6e3d
AdiBhat108 <jadithyabhat@gmail.com>
2021-02-08 20:49:28 +0530

    redux initial store and devtools setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
 import './App.css';
+import { BrowserRouter as Router,  Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar'; 
+import Landing from './components/layout/Landing'; 
+import Register from './components/auth/Register'; 
+import Login from './components/auth/Login';
+// Redux
+import { Provider } from 'react-redux';  
+import store from './store';
 
 const App = () => 
-  <>
-    <h1>App</h1>
-  </>
+  <Provider store={store}>
+    <Router>
+      <>
+        <Navbar />
+        <Route exact path ="/" component={Landing} />
+        <section className="container">
+          <Route exact path ="/register" component={Register} />
+          <Route exact path ="/login" component={Login} />
+        </section>
+      </>
+    </Router>
+  </Provider>
 export default App;
---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onchange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log("SUCCESS");
+  };
+
+  return (
+    <>
+      <p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            onChange={onchange}
+            value={email}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            onChange={onchange}
+            value={password}
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </>
+  )
+};
+
+export default Login;
