c38d73243cd91ccd20110903253bf3e427a84737
AdiBhat108 <jadithyabhat@gmail.com>
2021-02-12 23:56:30 +0530

    minor refactor

--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -5,7 +5,11 @@ export default () => (
   <>
     <img
       src={spinner}
-      style={{ width: '200px', margin: 'auto', display: 'block' }}
+      style={{
+        width: '200px',
+        margin: 'auto',
+        display: 'block',
+      }}
       alt="Loading..."
     />
   </>
