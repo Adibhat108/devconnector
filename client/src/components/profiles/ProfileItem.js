8b2e5bd768c9eb2c9429d8e4415e892f5b7a7276
AdiBhat108 <jadithyabhat@gmail.com>
2021-02-16 18:32:32 +0530

    removal of redundant code

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -1,5 +1,4 @@
-/* eslint-disable no-unused-vars */
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
