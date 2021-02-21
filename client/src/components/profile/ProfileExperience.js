7acbc28332e79df33f57ab2facd168cfd7e76755
AdiBhat108 <jadithyabhat@gmail.com>
2021-02-21 21:07:56 +0530

    removal of unused var

--- client/src/components/profile/ProfileExperience.js
+++ client/src/components/profile/ProfileExperience.js
@@ -1,11 +1,10 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'react-moment';
 
 const ProfileExperience = ({
   experience: {
-    company, title, location, current, to, from, description,
+    company, title, to, from, description,
   },
 }) => (
   <div>
