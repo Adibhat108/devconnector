15a3e4bf217e536a4401393051289c38076a80c9
AdiBhat108 <jadithyabhat@gmail.com>
2021-02-23 18:42:10 +0530

    addition of 404 handler page and routes

---
+++ client/src/components/layout/NotFound.js
@@ -0,0 +1,16 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <>
+      <h1 className="x-large text-primary">
+        <i className="fas fa-exclamation-triangle" />
+        Page not found
+      </h1>
+      <p className="large">Sorry, page not found.</p>
+    </>
+  );
+};
+
+export default NotFound;
